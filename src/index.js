23c9da4d0c054a1be04703239e69c126436fc488
michael-tawil <michaeltawil3@gmail.com>
2023-09-21 16:34:50 +1000

    mobile friendly done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 
 import mypic from "./me.jpg"
-console.log("Hello World!");
+import todoimg from "./todolist.png"
+import wimg from "./wimg.png"
+import libimg from "./lib.png"
+
 const meimg = document.querySelector("#mepic")
 meimg.src = mypic
+
+const todolistimg = document.querySelector("#todoimg")
+todolistimg.src = todoimg
+
+const weathapp = document.querySelector("#wapp")
+weathapp.src = wimg
+
+const libimmg = document.querySelector("#libimg")
+libimmg.src = libimg
+
 import "./style.css"
